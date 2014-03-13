de788591d0e4225d5a04bf6a905ed4d389e85972
sireesh <iamsireesh@gmail.com>
2014-03-13 22:23:44 +0530

    CI with Multiple Parameters: Args by data type

--- src/main/java/com/iamsireesh/ci/multipleparametrized/CIMultipleClient.java
+++ src/main/java/com/iamsireesh/ci/multipleparametrized/CIMultipleClient.java
@@ -9,7 +9,12 @@ public class CIMultipleClient {
 	public static void main(String[] args) {
 		Resource resource = new ClassPathResource("Spring.xml");
 		BeanFactory factory = new XmlBeanFactory(resource);
+		//Type 1: Arguments In Sequence
 		ConstructorMultiple obj = (ConstructorMultiple) factory.getBean("cimultiple");
 		System.out.println(obj.toString());
+		
+		//Type2: Arguments by using data type
+		ConstructorMultiple obj1 = (ConstructorMultiple) factory.getBean("cimultiple1");
+		System.out.println(obj1.toString());
 	}
 }
