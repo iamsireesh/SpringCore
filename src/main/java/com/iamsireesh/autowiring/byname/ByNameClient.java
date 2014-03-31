b507a6394375a8dc2f5a5b79943ddd945b9e5da9
sireesh <iamsireesh@gmail.com>
2014-03-31 22:00:27 +0530

    Autowiring ByName::SIOverrideCI

--- src/main/java/com/iamsireesh/autowiring/byname/ByNameClient.java
+++ src/main/java/com/iamsireesh/autowiring/byname/ByNameClient.java
@@ -10,6 +10,13 @@ public class ByNameClient {
 		Object object=context.getBean("byname");
 		ExampleInterface result=(ExampleBean)object;
 		System.out.println(result.show());
+		
+		
+		//Setter Injection Always dominate Constructor Injection
+		System.out.println("*************************");
+		Object object1=context.getBean("sidominateci");
+		ExampleInterface result1=(ExampleBean)object1;
+		System.out.println(result1.show());
 
 	}
 
